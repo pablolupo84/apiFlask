00a30e575effc668d150501f45766c3ed2646444
pablolupo84 <pablolupo84gmail.com>
2020-06-01 17:18:42 -0300

    Conexion a base de datos con SQLAlchemy

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG =True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/api'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development':DevelopmentConfig
