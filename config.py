52adc6fdfe4ed9c9026cb6a72d458b5d0634763a
pablolupo84 <pablolupo84gmail.com>
2020-06-09 17:37:41 -0300

    Pruebas unitarias

--- config.py
+++ config.py
@@ -6,6 +6,12 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/api'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class TestConfig(Config):
+    DEBUG =False
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/api_test'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 config = {
+    'test':TestConfig,
     'development':DevelopmentConfig
 }
